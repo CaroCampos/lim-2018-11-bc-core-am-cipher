8f90d5cc3c612383891e9b6f8d105987941d8473
carocampos <ccampossalazar@gmail.com>
2018-11-28 12:29:40 -0500

    División de archivos js Index y Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
+const cipher = () => {
+  string = document.getElementById('text').value;
+  offset = parseInt(document.getElementById('offset-position').value)%26;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      resultLetter += ' ';
+    } else {
+        const asciiLetter = (string.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
+        const letterCipher = String.fromCharCode(asciiLetter);
+        resultLetter += letterCipher;
+    }
+  } 
+  return resultLetter;
+};
+
+//////////////
+
+const decipher = () => {
+  string = document.getElementById('text').value;
+  offset = parseInt(document.getElementById('offset-position').value)%26;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      resultLetter += ' ';
+    } else {
+        const asciiLetter = (string.toUpperCase().charCodeAt(i) + 65 - offset) % 26 + 65;
+        const letterCipher = String.fromCharCode(asciiLetter);
+        resultLetter += letterCipher;
+    }
+  }
+  return resultLetter;
+};
+
+
 window.cipher = {
-  // ... 
+  encode: cipher,
+  decode: decipher
 };
--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
 let string = '';
 let offset =  '';
-
-const btnCipher = document.getElementById('btn-cipher');
-const btnDecipher = document.getElementById('btn-decipher');
-const btnEnter = document.getElementById('btn-enter')
-const resultMessage = document.getElementById('result-message');
-
 let resultLetter = '';
 
-//////////////
+const btnEnter = document.getElementById('btn-enter');
+const btnCipher = document.getElementById('btn-cipher');
+const btnDecipher = document.getElementById('btn-decipher');
+const resultMessage = document.getElementById('result-message');
 
-const cipher = () => {
-  string = document.getElementById('text').value;
-  offset = parseInt(document.getElementById('offset-position').value)%26;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === ' ') {
-      resultLetter += ' ';
-    } else {
-        const asciiLetter = (string.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
-        const letterCipher = String.fromCharCode(asciiLetter);
-        resultLetter += letterCipher;
-    }
-  } 
-  return resultLetter;
-};
+btnEnter.addEventListener('click', () => {
+  document.getElementById('section2').style.display = 'block';
+  document.getElementById('section1').style.display = 'none';
+});
 
 btnCipher.addEventListener('click', () => {
   string = document.getElementById('text').value;
   resultMessage.innerHTML = cipher(string);
 });
 
-/////////////
-
-const decipher = () => {
-  string = document.getElementById('text').value;
-  offset = parseInt(document.getElementById('offset-position').value)%26;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === ' ') {
-      resultLetter += ' ';
-    } else {
-        const asciiLetter = (string.toUpperCase().charCodeAt(i) + 65 - offset) % 26 + 65;
-        const letterCipher = String.fromCharCode(asciiLetter);
-        resultLetter += letterCipher;
-    }
-  }
-  return resultLetter;
-};
-
 btnDecipher.addEventListener('click', () => {
   string = document.getElementById('text').value;
   resultMessage.innerHTML = decipher(string);
 });
 
-btnEnter.addEventListener('click', () => {
-  document.getElementById('section2').style.display = 'block';
-  document.getElementById('section1').style.display = 'none';
-});
