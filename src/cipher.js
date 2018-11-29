8a313c66f1d7f56fc36535fbe9991e2c9495eb2a
carocampos <ccampossalazar@gmail.com>
2018-11-28 20:22:14 -0500

    Corección espacios y tabs en cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,18 @@
 window.cipher = {
-	encode: (string, offset) => {
+	encode:(string, offset) => {
 		let resultLetter = '';
 		for (let i = 0; i < string.length; i++) {
 			let asciiLetter = string.toUpperCase().charCodeAt(i);
-		    if (asciiLetter === 32) {
-		      	resultLetter += ' ';
-		    } else { 
-		      resultLetter += String.fromCharCode((asciiLetter - 65 + offset) % 26 + 65);
-		    }
+		if (asciiLetter === 32) {
+			resultLetter += ' ';
+		} else { 
+			resultLetter += String.fromCharCode((asciiLetter - 65 + offset) % 26 + 65);
+		}
 		} 
 		return resultLetter;
 	},
 
-  	decode: (string, offset) => {
+  	decode:(string, offset) => {
   		let resultLetter = '';
   		for (let i = 0; i < string.length; i++) {
   			let asciiLetter = string.toUpperCase().charCodeAt(i);
