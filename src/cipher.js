39ba6bf899c29a61e79e2afae6b6348870a5e533
carocampos <ccampossalazar@gmail.com>
2018-11-28 21:12:59 -0500

    Corección espacios y tabs en index.js y cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -3,29 +3,27 @@ window.cipher = {
 		let resultLetter = '';
 		for (let i = 0; i < string.length; i++) {
 			let asciiLetter = string.toUpperCase().charCodeAt(i);
-		if (asciiLetter === 32) {
-			resultLetter += ' ';
-		} else { 
-			resultLetter += String.fromCharCode((asciiLetter - 65 + offset) % 26 + 65);
+			if (asciiLetter === 32) {
+				resultLetter += ' ';
+			} else {
+				resultLetter += String.fromCharCode((asciiLetter - 65 + offset) % 26 + 65);
+			}
 		}
-		} 
 		return resultLetter;
 	},
-
-  	decode:(string, offset) => {
-  		let resultLetter = '';
-  		for (let i = 0; i < string.length; i++) {
-  			let asciiLetter = string.toUpperCase().charCodeAt(i);
-		    if (asciiLetter === 32) {
-		      	resultLetter += ' ';
-		    } else { 
-		      resultLetter += String.fromCharCode((asciiLetter + 65 - offset) % 26 + 65);
-		    }
-		} 
+	decode:(string, offset) => {
+		let resultLetter = '';
+		for (let i = 0; i < string.length; i++) {
+			let asciiLetter = string.toUpperCase().charCodeAt(i);
+			if (asciiLetter === 32) {
+				resultLetter += ' ';
+			} else {
+				resultLetter += String.fromCharCode((asciiLetter + 65 - offset) % 26 + 65);
+			}
+		}
 		return resultLetter;
 	}
 };
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ const btnCipher = document.getElementById('btn-cipher');
 const btnDecipher = document.getElementById('btn-decipher');
 
 btnEnter.addEventListener('click', () => {
-  	document.getElementById('section2').style.display = 'block';
-  	document.getElementById('section1').style.display = 'none';
+	document.getElementById('section2').style.display = 'block';
+	document.getElementById('section1').style.display = 'none';
 });
 
 btnCipher.addEventListener('click', () => {
-  	const text = document.getElementById('text').value;
-  	const position = parseInt(document.getElementById('offset-position').value);
-  	const resultMessage = (cipher.encode(text, position));
-  	document.getElementById('result-message').innerHTML = resultMessage;
+	const text = document.getElementById('text').value;
+	const position = parseInt(document.getElementById('offset-position').value);
+	const resultMessage = (cipher.encode(text, position));
+	document.getElementById('result-message').innerHTML = resultMessage;
 });
 
 btnDecipher.addEventListener('click', () => {
-  	const text2 = document.getElementById('text').value;
-  	const position2 = parseInt(document.getElementById('offset-position').value);
-  	const resultMessage2 = (cipher.decode(text2, position2));
-  	document.getElementById('result-message').innerHTML = resultMessage2;
+	const text2 = document.getElementById('text').value;
+	const position2 = parseInt(document.getElementById('offset-position').value);
+	const resultMessage2 = (cipher.decode(text2, position2));
+	document.getElementById('result-message').innerHTML = resultMessage2;
 });
